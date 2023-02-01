75ddbe4662422652ae18fb43b97f517c6a273880
Enesaydi <3n3say@gmail.com>
2023-02-01 16:52:32 +0300

    switch between pic.s

---
+++ Mathilda/31 ocak/js/index1.js
@@ -0,0 +1,16 @@
+function test1(){
+    alert("dosyadan bağlantı kuruldu.");
+}
+
+function bir(){
+    document.getElementById("x").src="img/1.jpeg";
+
+}
+
+function iki(){
+    document.getElementById("x").src="img/2.jpeg";
+}
+
+function uc(){
+    document.getElementById("x").src="img/3.jpeg";
+}
