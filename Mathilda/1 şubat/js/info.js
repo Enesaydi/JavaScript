155bd7e4c7c304b1149fde2506507d8ff5a3ec50
Enesaydi <3n3say@gmail.com>
2023-02-02 13:51:35 +0300

    getbootstrap

---
+++ Mathilda/1 şubat/js/info.js
@@ -0,0 +1,12 @@
+/*let a=54;
+
+document.getElementById("xx").innerHTML=a;
+alert(a);
+
+var b=233;
+b=83;
+document.getElementById("xx").innerHTML=b;
+*/
+const c=65;
+
+document.getElementById("xx").innerHTML=c;
