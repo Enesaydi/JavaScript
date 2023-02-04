f08b2c6d1a9704a5fc4821845f2fec09b997e635
Enesaydi <3n3say@gmail.com>
2023-02-04 23:48:51 +0300

    MouseOver etc. change style

---
+++ Mathilda/3 şubat/js/index1.js
@@ -0,0 +1,17 @@
+function yaps(){
+  //  alert("akjdbvhbfduv");
+document.getElementById("hh").innerHTML="bu alana yapştırma yapamazsın";
+ document.getElementById("hh").style.color="red";
+}
+
+function mouse(){
+    document.getElementById("pp").style.color="red";
+    document.getElementById("pp").style.fontSize="20px";
+    
+}
+
+function mouseo(){
+    document.getElementById("pp").style.color="yellow";
+    document.getElementById("pp").style.fontSize="30px";
+    document.getElementById("pp").innerHTML="Yazıyı değiştirebilirsin";
+}
