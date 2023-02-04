1669af0fbad411c90077f5d0f3671fede7e599c3
Enesaydi <3n3say@gmail.com>
2023-02-05 00:25:50 +0300

    CALCULATOR

---
+++ Mathilda/3 şubat/js/info.js
@@ -0,0 +1,20 @@
+function topl(){
+    var x=document.getElementById("ii").value;
+    var y=document.getElementById("iii").value;
+    document.getElementById("oo").innerHTML=Number(x)+Number(y);
+}
+function cık(){
+    var x=document.getElementById("ii").value;
+    var y=document.getElementById("iii").value;
+    document.getElementById("oo").innerHTML=Number(x)-Number(y);
+}
+function car(){
+    var x=document.getElementById("ii").value;
+    var y=document.getElementById("iii").value;
+    document.getElementById("oo").innerHTML=Number(x)*Number(y);
+}
+function bol(){
+    var x=document.getElementById("ii").value;
+    var y=document.getElementById("iii").value;
+    document.getElementById("oo").innerHTML=Number(x)/Number(y);
+}
