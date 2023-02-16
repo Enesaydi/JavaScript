ab16205c2aee7a7b94f2d25e9b4c545c8411f3ca
Enesaydi <3n3say@gmail.com>
2023-02-16 14:30:47 +0300

    java intro var-const-let

---
+++ noah/1/introj.js
@@ -0,0 +1,37 @@
+console.log("lsmfl");
+
+const sayi=32;
+console.log(sayi);
+console.log(typeof sayi);
+const ds="var eski bu yeni";
+console.log(ds);
+const deger="değişmez final gibi";
+console.log(deger);
+
+const d1=32;
+const d2="32";
+console.log(d1+d2);
+
+
+{let e=3;
+let r=4;
+//r=r+1; 
+let result=e+r;
+console.log(result);
+console.log(++result);}
+
+hop=45; //var olarak tanıdı 
+console.log(hop);
+
+var x=35;
+console.log(x);
+{var y=45;
+console.log(y);}
+var res=x+y;
+console.log(res);
+
+let e=3;
+let r=4;
+r=r+1; 
+let result=e+r;
+console.log(result);
