9fb4e55e96748d99fbd5897e6d9cccab3135053f
Enesaydi <3n3say@gmail.com>
2023-02-28 11:03:24 +0300

    function

---
+++ noah/6/while1.js
@@ -0,0 +1,12 @@
+
+function pr(){
+    console.log("hello")
+}
+
+pr()
+
+function oddoreven(num){return num % 2==0 ? "even" : "odd"}  
+console.log(oddoreven(6));
+
+function oddoreven(num){return num % 2==0 ? "even" : "odd"}  
+console.log(oddoreven(7));
